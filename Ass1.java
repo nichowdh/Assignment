840f91dfc1f6674382d10a8be26cbcc3c4461045
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2022-09-26 12:11:31 +0530

    Add files via upload

---
+++ Ass1.java
@@ -0,0 +1,7 @@
+class Ass1{
+public static void main(String args[]){
+float a=5.0f;
+float b=6.0f;
+System.out.println(a*b);
+}
+}
---
+++ Ass2.java
@@ -0,0 +1,8 @@
+//Java Program to Add two Numbers
+class Ass2{
+public static void main(String args[]){
+int a=2;
+int b=6;
+System.out.println(a+b);
+}
+}
