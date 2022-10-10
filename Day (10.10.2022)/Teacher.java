0d30dc880567dab8005389d83ce5e0c4c57ca8cb
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2022-10-10 14:33:31 +0530

    Add files via upload

---
+++ Day (10.10.2022)/Teacher.java
@@ -0,0 +1,23 @@
+package com;
+
+/*In this example, you have a base class Teacher and a 
+sub class ITTeacher. Since class ITTeacher extends the designation
+and college properties and work () method from base class,
+we need not to declare these properties and method in sub class.
+Here we have college Name, designation and work () method 
+which are common to all the teachers so we have 
+declared them in the base class,this way the child classes 
+like Math Teacher, Music Teacher and PhysicsTeacher 
+do not need to write this code and can be used directly 
+from base class.*/
+public class Teacher { // Base class
+	String designation, college, work;
+
+	public Teacher(String designation, String college, String work) { // constructor
+		this.designation = designation;
+		this.college = college;
+		this.work = work;
+
+	}
+
+}
